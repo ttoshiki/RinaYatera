e61b7d6e73473974917ddb4103fad84c60188938
t-tamaru <t-tamaru@sakura.ad.jp>
2019-10-29 22:14:56 +0900

    console.log削除

--- wp-content/themes/rinayatera/js/style.js
+++ wp-content/themes/rinayatera/js/style.js
@@ -1,12 +1,11 @@
 var now_post_num = 7; // 現在表示されている数
 var get_post_num = 2; // もっと読むボタンを押した時に取得する数
 
-jQuery("#moreLoad").on("click", function() {
-    console.log('a')
+$("#moreLoad").on("click", function() {
 
-    jQuery("#moreLoad").html('<img class="ajax_loading" src="http://rinayatera.com/wp/wp-content/themes/okuda/img/ajax-loader.gif" />');
+    $("#moreLoad").html('<img class="ajax_loading" src="http://rinayatera.com/wp/wp-content/themes/okuda/img/ajax-loader.gif" />');
 
-    jQuery.ajax({
+    $.ajax({
         type: 'post',
         url: 'http://rinayatera.com/wp/wp-content/themes/rinayatera/moreLoad.php',
         data: {
@@ -16,10 +15,10 @@ jQuery("#moreLoad").on("click", function() {
         success: function(data) {
             now_post_num = now_post_num + get_post_num;
             data = JSON.parse(data);
-            jQuery(".photos-thumbnail").append(data['html']);
-            jQuery("#moreLoad").remove();
+            $(".photos-thumbnail").append(data['html']);
+            $("#moreLoad").remove();
             if (!data['noDataFlg']) {
-                jQuery(".main").append('<li class="photos pc-img odd photos-hover my-effect"><button id="moreLoad">もっと読みこむ</button></li>');
+                $(".main").append('<li class="photos pc-img odd photos-hover my-effect"><button id="moreLoad">もっと読みこむ</button></li>');
             }
         }
     });
