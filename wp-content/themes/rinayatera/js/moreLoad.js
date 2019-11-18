c0cc567878fc61bee93a3e4b65e8a8c1585cb395
t-tamaru <t-tamaru@sakura.ad.jp>
2019-11-18 18:35:11 +0900

    ボタンのfadeOut実装

--- wp-content/themes/rinayatera/js/moreLoad.js
+++ wp-content/themes/rinayatera/js/moreLoad.js
@@ -20,15 +20,15 @@ let venoBox = function() {
 }
 venoBox()
 
-const vboxPreloadImage = '<div class="vbox-preloader"><div class="sk-wandering-cubes -gallery"><div class="sk-cube sk-cube1 -gallery" style="background-color: rgb(9, 137, 141);"></div><div class="sk-cube sk-cube2 -gallery" style="background-color: rgb(9, 137, 141);"></div></div></div>'
+const VBOX_PRELOAD_IMAGE = '<div class="vbox-preloader"><div class="sk-wandering-cubes -gallery"><div class="sk-cube sk-cube1 -gallery" style="background-color: rgb(9, 137, 141);"></div><div class="sk-cube sk-cube2 -gallery" style="background-color: rgb(9, 137, 141);"></div></div></div>'
 
 let addPhotosDom = function() {
-    const evenTransitionSpeed = 1600;
-    const oddTransitionSpeed = 800;
-    const fadeInSpeed = 100
+    const EVEN_TRANSITION_SPEED = 1600;
+    const ODD_TRANTISION_SPEED = 800;
+    const FADE_IN_SPEED = 100
     rangeToRoad = getJSONResults.slice(now_post_num + 1, total_post_num)
-    $('#moreLoad').remove()
-    $('#moreLoadButton').remove()
+    $('#moreLoad').fadeOut(200, function() { $(this).remove(); })
+    $('#moreLoadButton').fadeOut(200, function() { $(this).remove(); })
     $.each(rangeToRoad, function(i, item) {
         thumbnailUrl = item['_embedded']['wp:featuredmedia']['0']['media_details']['sizes']['full']['source_url']
         pcThumnailUrl = item['acf']['pc-thumbnail']
@@ -38,12 +38,12 @@ let addPhotosDom = function() {
         outputHtml += '<div class="view-more"><p class="view-more__sentense">view more</p></div></li>'
         $(outputHtml).appendTo("#artist-list").hide()
     });
-    $('.addDom:even').css({ position: 'relative', top: 20, opacity: 0, transition: evenTransitionSpeed + 'ms'});
-    $('.addDom:odd').css({ position: 'relative', top: 20, opacity: 0, transition: oddTransitionSpeed + 'ms'});
-    $('.addDom').fadeIn(fadeInSpeed).animate({
+    $('.addDom:even').css({ position: 'relative', top: 20, opacity: 0, transition: EVEN_TRANSITION_SPEED + 'ms'});
+    $('.addDom:odd').css({ position: 'relative', top: 20, opacity: 0, transition: ODD_TRANTISION_SPEED + 'ms'});
+    $('.addDom').fadeIn(FADE_IN_SPEED).animate({
         top: 0,
         opacity: 1
-    },200)
+    },300)
     venoBox()
 }
 
@@ -55,11 +55,11 @@ if (userAgent.indexOf("iPhone") >= 0 || userAgent.indexOf("iPad") >= 0 || userAg
     buttonText += 'CLICK '
 }
 buttonText += 'FOR MORE'
-const buttonHtml = '<li class="photos pc-img photos-thumbnail addDom" id="moreLoad"><button id="moreLoadButton"><span class="moreLoadButton__text">CLICK<br>FOR MORE</span></button></li>'
+const BUTTON_HTML = '<li class="photos pc-img photos-thumbnail addDom" id="moreLoad"><button id="moreLoadButton"><span class="moreLoadButton__text">CLICK<br>FOR MORE</span></button></li>'
 
 $("#artist-list").on("click", "#moreLoad", function() {
     $('.moreLoadButton__text').text('LOADING')
-    $('.moreLoadButton__text').append(vboxPreloadImage)
+    $('.moreLoadButton__text').append(VBOX_PRELOAD_IMAGE)
     $('#moreLoadButton').prop("disabled", true)
     if(isFirst) {
         $.getJSON( "//rinayatera.com/wp/wp-json/wp/v2/posts?_embed&categories=3&per_page=100", function(results) {
@@ -68,12 +68,12 @@ $("#artist-list").on("click", "#moreLoad", function() {
             addPhotosDom()
             now_post_num += get_post_num
             total_post_num += get_post_num
-            $("#artist-list").append(buttonHtml);
+            $("#artist-list").append(BUTTON_HTML);
         });
     } else {
         addPhotosDom()
         if(rangeToRoad.length >= get_post_num - 1) {
-            $("#artist-list").append(buttonHtml);
+            $("#artist-list").append(BUTTON_HTML);
             now_post_num += get_post_num
             total_post_num += get_post_num
         }
