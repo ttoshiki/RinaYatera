45787197fcce4bfd696ce5d48b78f68188bd2dea
momohayashi <twinkling.aspiration@gmail.com>
2019-12-01 23:40:43 +0900

    カスタムメニュー のスタイル設定

--- wp-content/themes/rinayatera/header.php
+++ wp-content/themes/rinayatera/header.php
@@ -49,7 +49,7 @@
   <?php if ( is_home() || is_front_page() ) : ?>
     <div class="header-inner">
          <h1 class="header-title">
-           <a href="../index.html">
+         <a href="index.php?page_id=2">
              <span class="header-logo text_left">rina yatera</span>
              <span class="header-logo__sub text_left">photographer</span>
            </a>
@@ -57,7 +57,7 @@
   <?php else: ?>
     <div class="header-inner">
          <p class="header-title">
-           <a href="../index.html">
+           <a href="index.php?page_id=2">
              <span class="header-logo text_left">rina yatera</span>
              <span class="header-logo__sub text_left">photographer</span>
            </a>
